3d36427ea891c8b77dcafeac9a92549ecfdd70e2
lucalai <lucaslaiz1245@gmail.com>
2024-07-05 18:04:21 -0300

    actualizo mongodb config

--- api-rest/src/config/mongodb.config.ts
+++ api-rest/src/config/mongodb.config.ts
@@ -1,21 +1,17 @@
-import {config as dotenvConfig} from 'dotenv';
+import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig({ path: './.env.development.local' });
 
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-
-const URI = `${process.env.MONGODB_HOST}+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.jafvbnl.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=Micluster`
-
+const URI = process.env.MONGODB_URI;
 
 const mongoConnect = async () => {
-    try {
-        await mongoose.connect(URI);
-    } catch (error) {
-        console.log(error);
-        
-    }
-   
-}
+  try {
+    await mongoose.connect(URI);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export {mongoConnect};
+export { mongoConnect };
